c75cfa5d5cf1a695411a63216140fc2afdba5725
Jester <jester.dash@gmail.com>
2016-09-12 10:17:31 +0300

    Add http port

--- lita_config.rb
+++ lita_config.rb
@@ -33,4 +33,6 @@ Lita.configure do |config|
   ## Example: Set configuration for any loaded handlers. See the handler's
   ## documentation for options.
   # config.handlers.some_handler.some_config_key = "value"
+
+  config.http.port = ENV['PORT']
 end
